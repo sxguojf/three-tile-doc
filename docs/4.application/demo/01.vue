2b653e5c9a4051a5e09751e59a43fb73d81ddf2d
hz_gjf@163.com <gjf123456>
2025-06-04 00:22:18 +0800

    -

--- docs/4.application/demo/01.vue
+++ docs/4.application/demo/01.vue
@@ -1,183 +1,185 @@
 <script setup lang="ts">
-import {
-    CubeTextureLoader,
-    Group,
-    Mesh,
-    MeshPhongMaterial,
-    SphereGeometry,
-    TextureLoader,
-    Vector2,
-    Vector3,
-    Color,
-} from "three";
-import * as tt from "three-tile";
-import * as plugin from "three-tile/plugin";
-import { Easing, Tween } from "three/examples/jsm/libs/tween.module.js";
-import Stats from "three/examples/jsm/libs/stats.module.js";
-import { onMounted, onUnmounted, reactive, ref, watch } from "vue";
-import { PLViewer } from "./PLViewer";
+    import { onMounted, onUnmounted, reactive, ref, watch } from "vue";
+    import {
+        CubeTextureLoader,
+        Group,
+        Mesh,
+        MeshPhongMaterial,
+        SphereGeometry,
+        TextureLoader,
+        Vector2,
+        Vector3,
+        Color,
+    } from "three";
+    import { Easing, Tween } from "three/addons/libs/tween.module.js";
+    import Stats from "three/addons/libs/stats.module.js";
 
-const mapRef = ref<HTMLDivElement>();
-const autoForward = ref<boolean>(false);
+    import * as tt from "three-tile";
+    import * as plugin from "three-tile/plugin";
+    import { PLViewer } from "./PLViewer";
 
-const state = reactive({
-    fireCount: 0,
-    location: new Vector3(),
-    loading: 0,
-});
+    const mapRef = ref<HTMLDivElement>();
+    const autoForward = ref<boolean>(false);
 
-// 创建地图
-const createMap = () => {
-    const map = new tt.TileMap({
-        // 影像数据源
-        imgSource: new plugin.ArcGisSource(),
-        // 地形数据源
-        demSource: new plugin.ArcGisDemSource(),
-        // 中央子午线经度
-        lon0: 90,
-        minLevel: 8,
-        debug: 1,
+    const state = reactive({
+        fireCount: 0,
+        location: new Vector3(),
+        loading: 0,
     });
-    // 地图旋转到xz平面
-    map.rotateX(-Math.PI / 2);
 
-    return map;
-};
+    // 创建地图
+    const createMap = () => {
+        const map = new tt.TileMap({
+            // 影像数据源
+            imgSource: new plugin.ArcGisSource(),
+            // 地形数据源
+            demSource: new plugin.ArcGisDemSource(),
+            // 中央子午线经度
+            lon0: 90,
+            minLevel: 8,
+            debug: 1,
+        });
+        // 地图旋转到xz平面
+        map.rotateX(-Math.PI / 2);
 
-onMounted(() => {
-    if (!mapRef.value) return;
+        return map;
+    };
 
-    // 地图
-    const map = createMap();
+    onMounted(() => {
+        if (!mapRef.value) return;
 
-    // 场景
-    const viewer = new PLViewer(mapRef.value);
-    const { scene, camera, controls } = viewer;
+        // 地图
+        const map = createMap();
 
-    //添加蓝天白云
-    const skybox = new CubeTextureLoader()
-        .setPath("../skybox4/")
-        .load(["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]);
-    scene.background = skybox;
-    viewer.scene.fog.color = new Color(0xd4dcea);
-    viewer.scene.fogFactor = 0.9;
-    // 调整相机位置
-    const cameraPosition = map.geo2world(new Vector3(86, 28, 1e4));
-    camera.position.copy(cameraPosition);
+        // 场景
+        const viewer = new PLViewer(mapRef.value);
+        const { scene, camera, controls } = viewer;
 
-    scene.add(map);
-
-    viewer.container?.addEventListener("click", (evt) => {
-        if (!controls.isLocked) {
-            controls.lock();
-        } else if (evt.button === 2) {
-            controls.unlock();
+        //添加蓝天白云
+        const skybox = new CubeTextureLoader()
+            .setPath("../skybox4/")
+            .load(["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]);
+        scene.background = skybox;
+        if (viewer.scene.fog) {
+            viewer.scene.fog.color = new Color(0xd4dcea);
         }
+        // 调整相机位置
+        const cameraPosition = map.geo2world(new Vector3(86, 28, 1e4));
+        camera.position.copy(cameraPosition);
+
+        scene.add(map);
+
+        viewer.container?.addEventListener("click", (evt) => {
+            if (!controls.isLocked) {
+                controls.lock();
+            } else if (evt.button === 2) {
+                controls.unlock();
+            }
+        });
+
+        watch(autoForward, (v) => (viewer.autoForward = v));
+
+        map.addEventListener("update", () => {
+            const focus = map.getLocalInfoFromScreen(camera, new Vector2(0, 0));
+            if (focus) {
+                const ll = map.world2geo(focus.point);
+                state.location.copy(ll);
+            }
+            state.loading = map.downloading;
+        });
+
+        addStats(viewer);
+
+        fireInit(viewer, map);
+
+        onUnmounted(() => {
+            map.dispose();
+        });
     });
 
-    watch(autoForward, (v) => (viewer.autoForward = v));
+    const fireInit = (viewer: PLViewer, map: tt.TileMap) => {
+        const { camera, container, cameraHeight, controls } = viewer;
 
-    map.addEventListener("update", () => {
-        const focus = map.getLocalInfoFromScreen(camera, new Vector2(0, 0));
-        if (focus) {
-            const ll = map.world2geo(focus.point);
-            state.location.copy(ll);
-        }
-        state.loading = map.downloading;
-    });
+        const bombGroup = new Group();
+        map.add(bombGroup);
 
-    addStats(viewer);
+        // 创建一个球
+        const ball = new Mesh(
+            new SphereGeometry(),
+            new MeshPhongMaterial({
+                map: new TextureLoader().load("../test.jpg"),
+                shininess: 3,
+                transparent: true,
+            })
+        );
 
-    fireInit(viewer, map);
+        // 监听click
+        container?.addEventListener("click", (evt) => {
+            if (!controls.isLocked || evt.button != 0) {
+                return;
+            }
 
-    onUnmounted(() => {
-        map.dispose();
-    });
-});
+            state.fireCount++;
 
-const fireInit = (viewer: PLViewer, map: tt.TileMap) => {
-    const { camera, container, cameraHeight, controls } = viewer;
+            // clone一个球
+            const aBall = ball.clone();
+            aBall.scale.setScalar(cameraHeight / 40);
+            bombGroup.add(aBall);
 
-    const bombGroup = new Group();
-    map.add(bombGroup);
+            // 球起点坐标：从摄像机位置开始
+            const startPosition = map.worldToLocal(camera.getWorldPosition(new Vector3()));
+            aBall.position.copy(startPosition);
 
-    // 创建一个球
-    const ball = new Mesh(
-        new SphereGeometry(),
-        new MeshPhongMaterial({
-            map: new TextureLoader().load("../test.jpg"),
-            shininess: 3,
-            transparent: true,
-        })
-    );
+            // 取得视线与地面交点
+            const crossPoint = map.getLocalInfoFromScreen(camera, new Vector2(0, 0));
+            // 球终点坐标：视线与地面有交点则取交点，无交点则取摄像机前方100km
+            const endPostion = crossPoint
+                ? map.worldToLocal(crossPoint.point)
+                : map.worldToLocal(camera.localToWorld(new Vector3(0, 0, -100 * 1000)));
 
-    // 监听click
-    container?.addEventListener("click", (evt) => {
-        if (!controls.isLocked || evt.button != 0) {
-            return;
-        }
+            // 球飞行时间
+            const delay = crossPoint ? Math.log(crossPoint.distance) * 100 : 1000;
+            // 球直线飞行动画
+            new Tween(aBall.position).to(endPostion, delay).easing(Easing.Quadratic.Out).start();
+            // 球边飞边转
+            new Tween(aBall.rotation).to({ x: Math.PI, y: Math.PI, z: Math.PI }, delay).start();
 
-        state.fireCount++;
+            // 球减速下落动画(模拟重力作用)
+            new Tween(aBall.position)
+                .to({ z: endPostion.z }, delay)
+                .easing(crossPoint ? Easing.Quadratic.In : Easing.Quartic.Out)
+                .start()
+                .onComplete(() => {
+                    // 动画完成延迟销毁球
+                    const dispose = () => {
+                        aBall.removeFromParent();
+                        aBall.geometry.dispose();
+                        aBall.material.dispose();
+                    };
+                    if (crossPoint) {
+                        setTimeout(dispose, 10 * delay);
+                    } else {
+                        dispose();
+                    }
+                });
 
-        // clone一个球
-        const aBall = ball.clone();
-        aBall.scale.setScalar(cameraHeight / 40);
-        bombGroup.add(aBall);
+            // 后坐力
+            const pos = camera.position.clone();
+            camera.position.sub(camera.getWorldDirection(new Vector3()).multiplyScalar(1000));
+            setTimeout(() => {
+                camera.position.copy(pos);
+            }, 50);
+        });
+    };
 
-        // 球起点坐标：从摄像机位置开始
-        const startPosition = map.worldToLocal(camera.getWorldPosition(new Vector3()));
-        aBall.position.copy(startPosition);
-
-        // 取得视线与地面交点
-        const crossPoint = map.getLocalInfoFromScreen(camera, new Vector2(0, 0));
-        // 球终点坐标：视线与地面有交点则取交点，无交点则取摄像机前方100km
-        const endPostion = crossPoint
-            ? map.worldToLocal(crossPoint.point)
-            : map.worldToLocal(camera.localToWorld(new Vector3(0, 0, -100 * 1000)));
-
-        // 球飞行时间
-        const delay = crossPoint ? Math.log(crossPoint.distance) * 100 : 1000;
-        // 球直线飞行动画
-        new Tween(aBall.position).to(endPostion, delay).easing(Easing.Quadratic.Out).start();
-        // 球边飞边转
-        new Tween(aBall.rotation).to({ x: Math.PI, y: Math.PI, z: Math.PI }, delay).start();
-
-        // 球减速下落动画(模拟重力作用)
-        new Tween(aBall.position)
-            .to({ z: endPostion.z }, delay)
-            .easing(crossPoint ? Easing.Quadratic.In : Easing.Quartic.Out)
-            .start()
-            .onComplete(() => {
-                // 动画完成延迟销毁球
-                const dispose = () => {
-                    aBall.removeFromParent();
-                    aBall.geometry.dispose();
-                    aBall.material.dispose();
-                };
-                if (crossPoint) {
-                    setTimeout(dispose, 10 * delay);
-                } else {
-                    dispose();
-                }
-            });
-
-        // 后坐力
-        const pos = camera.position.clone();
-        camera.position.sub(camera.getWorldDirection(new Vector3()).multiplyScalar(1000));
-        setTimeout(() => {
-            camera.position.copy(pos);
-        }, 50);
-    });
-};
-
-// 添加性能监视器
-function addStats(viewer: PLViewer) {
-    const stats = new Stats();
-    stats.showPanel(0);
-    stats.dom.style.position = "relative";
-    document.querySelector("#stats")?.appendChild(stats.dom);
-    viewer.addEventListener("update", () => stats.update());
-}
+    // 添加性能监视器
+    function addStats(viewer: PLViewer) {
+        const stats = new Stats();
+        stats.showPanel(0);
+        stats.dom.style.position = "relative";
+        document.querySelector("#stats")?.appendChild(stats.dom);
+        viewer.addEventListener("update", () => stats.update());
+    }
 </script>
 
 <template>
@@ -201,48 +203,48 @@ function addStats(viewer: PLViewer) {
 </template>
 
 <style scoped>
-.map-container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    color: #ffffff;
-    height: 600px;
-}
-.map {
-    width: 100%;
-    height: 100%;
-}
+    .map-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        color: #ffffff;
+        height: 600px;
+    }
+    .map {
+        width: 100%;
+        height: 100%;
+    }
 
-.focus {
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%, 0%);
-    filter: drop-shadow(0 0 3px #ffffff);
-    pointer-events: none;
-}
-.mapState {
-    position: absolute;
-    right: 30px;
-    top: 80px;
-    text-shadow: 1px 1px 1px black;
-}
-#help {
-    position: absolute;
-    left: 30px;
-    top: 80px;
-    text-shadow: 1px 1px 1px black;
-}
-#stats {
-    position: absolute;
-    top: 610px;
-    left: 30px;
-    z-index: 1000;
-}
-.forward {
-    position: absolute;
-    right: 30px;
-    top: 630px;
-    z-index: 1000;
-}
+    .focus {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 0%);
+        filter: drop-shadow(0 0 3px #ffffff);
+        pointer-events: none;
+    }
+    .mapState {
+        position: absolute;
+        right: 30px;
+        top: 80px;
+        text-shadow: 1px 1px 1px black;
+    }
+    #help {
+        position: absolute;
+        left: 30px;
+        top: 80px;
+        text-shadow: 1px 1px 1px black;
+    }
+    #stats {
+        position: absolute;
+        top: 610px;
+        left: 30px;
+        z-index: 1000;
+    }
+    .forward {
+        position: absolute;
+        right: 30px;
+        top: 630px;
+        z-index: 1000;
+    }
 </style>
