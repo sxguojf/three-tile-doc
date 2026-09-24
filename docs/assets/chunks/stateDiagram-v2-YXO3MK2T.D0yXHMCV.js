8f9c8ff5cdc8bc7ee5f0a5f68d59c7e4ad247992
hz_gjf@163.com <gjf123456>
2026-09-24 23:49:10 +0800

    -

---
+++ docs/assets/chunks/stateDiagram-v2-YXO3MK2T.D0yXHMCV.js
@@ -0,0 +1,1 @@
+import{s as e,b as r,a,S as s}from"./chunk-AEK57VVT.CR5Xa7lJ.js";import{_ as i}from"../app.DH8VMAJk.js";import"./chunk-RZ5BOZE2.rdFOJqWd.js";import"./framework.xbTv8SNN.js";import"./theme.BYoDEJjJ.js";var p={parser:a,get db(){return new s(2)},renderer:r,styles:e,init:i(t=>{t.state||(t.state={}),t.state.arrowMarkerAbsolute=t.arrowMarkerAbsolute},"init")};export{p as diagram};
